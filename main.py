1f51545a23aa84a5bea428090c23f5593d339b4e
harshoza24@gmail.com <harshoza24@gmail.com>
2017-12-01 21:09:02 -0500

    added template and css

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-x = 4
+def add_fare(filename):
+    d = {}
+    with open (filename) as text:
+        for line in text:
+            if lists[7].isalpha() or lists[8].isalpha():
+				pass
+            else:
 
-if x < 5:
-    print("hi")
+
+
+def rel_fares(filename, budget, num_people, origin):
+    d = {}
+    with open (filename) as text:
+        for line in text:
+        line = line.strip();
+        line = line.split(",")
+        return line
+
+print(rel_fares("Deals.csv", 700, 4, "Chicago"))
